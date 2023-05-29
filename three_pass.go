e78f896cb436eebeb0661a0bd7305149fa6ad5c1
Joshua Hull <josh@fireflop.com>
2023-05-29 17:31:42 +0200

    Move config to common

--- three_pass.go
+++ three_pass.go
@@ -70,61 +70,6 @@ type ThreePassJpake[P CurvePoint[P, S], S CurveScalar[S]] struct {
 
 // curve25519Curve{curve[curvePoint[curve25519point]]}
 
-type Config struct {
-	sessionConfirmationBytes []byte
-	secretGenerationBytes    []byte
-	sessionGenerationBytes   []byte
-	hashFn                   HashFnType
-	macFn                    MacFnType
-}
-
-func NewConfig() *Config {
-	return &Config{
-		sessionConfirmationBytes: []byte("JPAKE_CONFIRM"),
-		secretGenerationBytes:    []byte("SECRET"),
-		sessionGenerationBytes:   []byte("SESSION"),
-		hashFn:                   sha256HashFn,
-		macFn:                    hmacsha256KDF,
-	}
-}
-
-func (c *Config) SetSessionConfirmationBytes(scb []byte) *Config {
-	c.sessionConfirmationBytes = scb
-	return c
-}
-
-func (c *Config) SetSecretGenerationBytes(s []byte) *Config {
-	c.secretGenerationBytes = s
-	return c
-}
-
-func (c *Config) SetSessionGenerationBytes(s []byte) *Config {
-	c.sessionGenerationBytes = s
-	return c
-}
-
-func (c *Config) SetHashFn(h HashFnType) *Config {
-	c.hashFn = h
-	return c
-}
-
-func (c *Config) SetMacFn(f MacFnType) *Config {
-	c.macFn = f
-	return c
-}
-
-func (c *Config) generateSecret(pw []byte) []byte {
-	return c.hashFn(c.macFn(pw, c.secretGenerationBytes))
-}
-
-func (c *Config) generateConfirmationMac(k, msg []byte) []byte {
-	return c.macFn(c.macFn(k, c.sessionConfirmationBytes), msg)
-}
-
-func (c *Config) generateSessionKey(k []byte) []byte {
-	return c.macFn(k, c.sessionGenerationBytes)
-}
-
 func InitThreePassJpake(userID, pw []byte) (*ThreePassJpake[*Curve25519Point, *Curve25519Scalar], error) {
 	return InitThreePassJpakeWithConfig(userID, pw, NewConfig())
 }
